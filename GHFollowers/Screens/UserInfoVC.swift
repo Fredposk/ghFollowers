ba6e63a8a63067286b4a7a357275c02d97ac305e
Frederico Kuckelhaus <freddy.bdb@gmail.com>
2021-11-15 22:26:48 +0100

    refactored User viewcontroller

--- GHFollowers/Screens/UserInfoVC.swift
+++ GHFollowers/Screens/UserInfoVC.swift
@@ -12,16 +12,27 @@ class UserInfoVC: UIViewController {
     var userName: String!
 
     let headerView = UIView()
+    let itemOne = UIView()
+    let itemTwo = UIView()
+
+    var itemViews: [UIView] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemBackground
 
+        configureViewController()
+        layoutUI()
+        getUserInfo()
+
+    }
+
+    func configureViewController() {
+        view.backgroundColor = .systemBackground
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(dismissVC))
         navigationItem.rightBarButtonItem = doneButton
+    }
 
-
-        layoutUI()
+    func getUserInfo() {
         NetworkManager.shared.getUser(for: userName) { [weak self] result in
             guard let self = self else { return }
 
@@ -29,9 +40,9 @@ class UserInfoVC: UIViewController {
             case .failure(let error):
                 self.presentGFAlertOnMainThread(title: "\(error)", message: "Error fetching user", buttonTitle: "OK")
             case .success(let user):
-                
+
                 DispatchQueue.main.async {
-                    self.add(childVC: GFUserInfoHeaderVC(user: user), to: self.headerView)
+                    self.add(GFUserInfoHeaderVC(user: user), to: self.headerView)
                 }
 
             }
@@ -39,19 +50,54 @@ class UserInfoVC: UIViewController {
     }
 
     func layoutUI() {
-        view.addSubview(headerView)
-        headerView.translatesAutoresizingMaskIntoConstraints = false
+
+        let padding: CGFloat = 20
+        let itemHeight: CGFloat = 140
+
+        itemViews = [headerView, itemOne, itemTwo]
+
+        
+
+        for itemView in itemViews {
+            view.addSubview(itemView)
+            itemView.translatesAutoresizingMaskIntoConstraints = false
+
+            NSLayoutConstraint.activate([
+                itemView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+                itemView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding)
+            ])
+
+
+        }
+
+
+        itemOne.backgroundColor = .systemPink
+        itemTwo.backgroundColor = .systemBlue
+
 
 
         NSLayoutConstraint.activate([
             headerView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
-            headerView.heightAnchor.constraint(equalToConstant: 180)
+//            headerView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+//            headerView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            headerView.heightAnchor.constraint(equalToConstant: itemHeight + 40),
+
+            itemOne.topAnchor.constraint(equalTo: headerView.bottomAnchor, constant: padding),
+//            itemOne.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+//            itemOne.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            itemOne.heightAnchor.constraint(equalToConstant: itemHeight),
+
+            itemTwo.topAnchor.constraint(equalTo: itemOne.bottomAnchor, constant: padding),
+//            itemTwo.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: padding),
+//            itemTwo.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -padding),
+            itemTwo.heightAnchor.constraint(equalToConstant: itemHeight),
+
         ])
     }
 
-    func add(childVC: UIViewController, to containerView: UIView) {
+    /// https://www.swiftbysundell.com/basics/child-view-controllers/
+    /// best explanation
+    func add(_ childVC: UIViewController, to containerView: UIView) {
         addChild(childVC)
         containerView.addSubview(childVC.view)
         childVC.view.frame = containerView.bounds
